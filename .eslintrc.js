fac76e3ecb9d2c9a874f6479dcf4fa02b6108f7c
tony555t <your-email@example.com>
2025-09-23 13:23:38 +0300

    fix: update ESLint config to resolve no-undef errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,44 @@ module.exports = {
     browser: true,
     node: true,
     es2021: true,
-    jest: true
+    jest: true,
+    commonjs: true
   },
   extends: [
     'eslint:recommended'
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   rules: {
-    'no-undef': 'off'
+    'no-undef': 'off',
+    'no-unused-vars': 'warn'
   },
   globals: {
+    // Node.js globals
     require: 'readonly',
     process: 'readonly',
+    module: 'readonly',
+    exports: 'readonly',
+    __dirname: 'readonly',
+    __filename: 'readonly',
+    global: 'readonly',
+    Buffer: 'readonly',
+    console: 'readonly',
+    
+    // Jest/Testing globals
     test: 'readonly',
     expect: 'readonly',
     describe: 'readonly',
-    it: 'readonly'
+    it: 'readonly',
+    beforeEach: 'readonly',
+    afterEach: 'readonly',
+    beforeAll: 'readonly',
+    afterAll: 'readonly',
+    jest: 'readonly'
   }
 };
